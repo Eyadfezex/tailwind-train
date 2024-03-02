7295d1a89d5aab96edc07592337ca438892b071d
Eyad ahmed <Eyad932004@outloo.com>
2024-03-02 12:02:56 +0200

    the first commit

---
+++ src/sideBar.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { BsPlus, BsFillLightningFill, BsGearFill } from "react-icons/bs";
+import { FaFire, FaPoo } from "react-icons/fa";
+
+interface SideBar {
+  icon: any;
+  text: string;
+}
+
+function SideBar() {
+  return (
+    <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg">
+      <SideBarIcon icon={<FaFire size="28" />} text="Eyad" />
+      <SideBarIcon icon={<FaPoo size="28" />} text="Eyad" />
+      <SideBarIcon icon={<BsPlus size="28" />} text="Eyad" />
+      <SideBarIcon icon={<BsFillLightningFill size="28" />} text="Eyad" />
+      <SideBarIcon icon={<BsGearFill size="28" />} text="Eyad" />
+    </div>
+  );
+}
+
+const SideBarIcon = ({ icon, text }: SideBar) => {
+  return (
+    <div className="Sidebar-icon group">
+      {icon}
+      <span className="Sidebar-tooltip group-hover:scale-100">{text}</span>
+    </div>
+  );
+};
+export default SideBar;
