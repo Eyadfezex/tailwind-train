b3f56787757f2e6004de21fe44c97f1b002fc5ba
Eyad ahmed <Eyad932004@outloo.com>
2024-03-02 17:43:03 +0200

    add some docs

--- src/sideBar.tsx
+++ src/sideBar.tsx
@@ -1,12 +1,15 @@
+// Import icons from react-icons
 import React from "react";
 import { BsPlus, BsFillLightningFill, BsGearFill } from "react-icons/bs";
 import { FaFire, FaPoo } from "react-icons/fa";
 
+// Define the SideBar interface for the props
 interface SideBar {
   icon: any;
   text: string;
 }
 
+// SideBar component
 function SideBar() {
   return (
     <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg">
@@ -18,6 +21,7 @@ function SideBar() {
     </div>
   );
 }
+// SideBarIcon component
 
 const SideBarIcon = ({ icon, text }: SideBar) => {
   return (
@@ -27,4 +31,6 @@ const SideBarIcon = ({ icon, text }: SideBar) => {
     </div>
   );
 };
+
+// Export the SideBar component
 export default SideBar;
